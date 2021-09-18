0c04e26726a64e53fa8d2ffc30f1cfbe22903fdb
altynai91 <lenta.kg@gmail.com>
2021-09-18 03:48:05 +0300

    task: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ import { NotImplementedError } from '../extensions/index.js';
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const NATURAL_ALG_OF_TWO = 0.693;
 
 /**
  * Determine the age of archeological find by using
@@ -17,7 +18,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
+  let num = parseFloat(sampleActivity);
+  let up = Math.log(num / MODERN_ACTIVITY);
+  let down = NATURAL_ALG_OF_TWO / HALF_LIFE_PERIOD;
+  let yo = Math.abs(up) / down.toFixed(12); 
+  let result = Math.ceil(yo);
+  return (typeof(sampleActivity) == 'string' && sampleActivity > 0 && typeof(num) == 'number' && isNaN(num) == false && num < 15) ? result : false;
 }

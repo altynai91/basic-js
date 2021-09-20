44b76d1b3702c1a8701fafcadf1b45ff9468637d
altynai91 <lenta.kg@gmail.com>
2021-09-20 22:38:30 +0300

    task: hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = Math.pow(2, disksNumber) - 1;
+  let seconds = (turns / turnsSpeed) * 3600;
+  let options = {
+    'turns': turns,
+    'seconds': Math.floor(seconds)
+  };
+  return options;
 }
